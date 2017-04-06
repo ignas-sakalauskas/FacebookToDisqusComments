4a27de012993ed7911abc03cdadb62c0a1bcaa52
Ignas Sakalauskas <ignas.sakal@outlook.com>
2017-04-06 23:31:19 +0100

    Rename JsonParser tests file.

---
+++ FacebookToDisqusComments.Tests/DataServices/JsonParserTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FacebookToDisqusComments.DataServices;
+using FacebookToDisqusComments.ApiWrappers.Dtos;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FacebookToDisqusComments.Tests.DataServices
+{
+    [TestClass]
+    public class JsonParserTests
+    {
+        [DataTestMethod]
+        [DataRow(null, DisplayName = "Null")]
+        [DataRow("", DisplayName = "Empty")]
+        [DataRow(" ", DisplayName = "Whitespace")]
+        public void ParseJsonResponse_ShouldThrowArgumentNullException_WhenContentIsInvalid(string content)
+        {
+            // Arrange
+            var parser = new JsonParser();
+
+            // Act
+            Action action = () => parser.ParseJsonResponse<FacebookCommentsPage>(content);
+
+            // Assert
+            action.ShouldThrow<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void ParseJsonResponse_ShouldReturnNull_WhenContentIsEmptyJsonObject()
+        {
+            // Arrange
+            const string json = "{}";
+            var parser = new JsonParser();
+
+            // Act
+            var result = parser.ParseJsonResponse<FacebookCommentsPage>(json);
+
+            // Assert
+            result.Comments.Should().BeNull();
+        }
+    }
+}
